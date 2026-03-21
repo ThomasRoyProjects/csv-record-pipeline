6c4087fa4987fefd9960bc4cfed8a94f58543d0d
ThomasRoyAlgonquinCourse <thomasroy149@protonmail.com>
2026-03-20 23:04:32 -0700

    Remove unused nationbuilder export wrapper

--- export/nationbuilder.py
+++
@@ -1,9 +0,0 @@
-from export.records import project_records
-
-
-def project_to_nationbuilder(df):
-    """
-    Compatibility wrapper around the generic export projection.
-    """
-
-    return project_records(df)
